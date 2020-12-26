2ebfec1b32803938c72da71649659fec8820db22
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-12-26 00:15:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,40 +57,40 @@ class Student
       end
   end
 
-    def self.first_X_students_in_grade_10(num)
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = 10
-        LIMIT ?
-        SQL
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?
+      SQL
 
-        DB[:conn].execute(sql, num).map do |row|
-          self.new_from_db(row)
-        end
-    end
+      DB[:conn].execute(sql, num).map do |row|
+        self.new_from_db(row)
+      end
+  end
 
-    def self.first_student_in_grade_10
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = 10
-        LIMIT 1
-        SQL
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+      SQL
 
-        DB[:conn].execute(sql).map do |row|
-          self.new_from_db(row)
-        end.first
-    end
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end.first
+  end
 
-    def self.all_students_in_grade_X(grade)
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = ?
-        SQL
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      SQL
 
-        DB[:conn].execute(sql, grade).map do |row|
-          self.new_from_db(row)
-        end
-    end
+      DB[:conn].execute(sql, grade).map do |row|
+        self.new_from_db(row)
+      end
+  end
 
   def save
     sql = <<-SQL
